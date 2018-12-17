83b7de08f6a1f4cdf4d729740505851e5288ca55
CODE <sdluka@hotmail.com>
2018-12-17 17:56:44 +0100

    klasa, konstruktor, instance klase

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hello World2.");
+class Klijent {
+  constructor(ime, brojRacuna, stanje) {
+    this.ime = ime;
+    this.brojRacuna = brojRacuna;
+    this.stanje = stanje;
+  }
+  prenetiSredstva(primalac, iznos) {
+    if (this.stanje >= iznos) {
+      this.stanje -= iznos;
+      primalac.stanje += iznos;
+    } else console.log("Nemate dovoljno sredstava na racunu.");
+  }
+}
+
+const petar = new Klijent("Petar Petrovic", 1600033331, 2000);
+const ana = new Klijent("Ana Pavlovic", 1600033332, 15000);
+const jovana = new Klijent("Jovana",1600033333, 6000);
